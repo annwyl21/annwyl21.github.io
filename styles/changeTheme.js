664e2bdb51610a08189cb1540616c0d5ab0524bc
Ellen Houghton <ellen.a.ash@gmail.com>
2023-06-07 18:10:29 +0100

    add js script

--- styles/changeTheme.js
+++ styles/changeTheme.js
@@ -1,4 +1,4 @@
-<script>
+{/* <script>
     document.getElementById('themeButton').addEventListener('click', function() {
         var stylesheet = document.getElementById('stylesheet');
         if (stylesheet.getAttribute('href') == 'styles.css') {
@@ -9,5 +9,14 @@
             this.innerText = "Switch to Dark Mode";
         }
     });
-</script>
+</script> */}
+
+// function disclaimer() {
+//     document.getElementById("disclaimer").innerHTML="DISCLAIMER: This website was designed by programming students and while useful, does not offer financial advice.";
+// }
+
+// function disclaimer() {
+//     confirm("DISCLAIMER: This website was designed by a student programmer and while useful, does not offer financial advice.")
+//     document.getElementById("disclaimer")
+// }
 
