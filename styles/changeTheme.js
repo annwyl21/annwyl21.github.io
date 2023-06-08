7e50e8da68694351e48e7676c95ee43b21a3df66
Ellen Houghton <ellen.a.ash@gmail.com>
2023-06-08 10:37:44 +0100

    add light and dark mode button

--- styles/changeTheme.js
+++ styles/changeTheme.js
@@ -1,22 +1,11 @@
-/* <script>
+
     document.getElementById('themeButton').addEventListener('click', function() {
         var stylesheet = document.getElementById('stylesheet');
-        if (stylesheet.getAttribute('href') == 'styles.css') {
-            stylesheet.setAttribute('href', 'style_dark.css');
+        if (stylesheet.getAttribute('href') == './styles/styles.css') {
+            stylesheet.setAttribute('href', './styles/style_dark.css');
             this.innerText = "Switch to Light Mode";
         } else {
-            stylesheet.setAttribute('href', 'styles.css');
+            stylesheet.setAttribute('href', './styles/styles.css');
             this.innerText = "Switch to Dark Mode";
         }
     });
-</script> */
-
-// function disclaimer() {
-//     document.getElementById("disclaimer").innerHTML="DISCLAIMER: This website was designed by programming students and while useful, does not offer financial advice.";
-// }
-
-// function disclaimer() {
-//     confirm("DISCLAIMER: This website was designed by a student programmer and while useful, does not offer financial advice.")
-//     document.getElementById("disclaimer")
-// }
-
