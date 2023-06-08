156385bdfa3111b824d7c62ab4e49a0cc9ae9833
Ellen Houghton <ellen.a.ash@gmail.com>
2023-06-08 10:50:49 +0100

    change box to be slider switch for dark theme

--- styles/changeTheme.js
+++ styles/changeTheme.js
@@ -3,9 +3,9 @@
         var stylesheet = document.getElementById('stylesheet');
         if (stylesheet.getAttribute('href') == './styles/styles.css') {
             stylesheet.setAttribute('href', './styles/style_dark.css');
-            this.innerText = "Switch to Light Mode";
+            // this.innerText = "Switch to Light Mode";
         } else {
             stylesheet.setAttribute('href', './styles/styles.css');
-            this.innerText = "Switch to Dark Mode";
+            // this.innerText = "Switch to Dark Mode";
         }
     });
