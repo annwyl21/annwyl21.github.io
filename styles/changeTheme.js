2eab88bd284c599347f9f2790e927e78602902f7
Ellen Houghton <ellen.a.ash@gmail.com>
2023-06-08 12:05:08 +0100

    create general style sheet and add specific light and dark theme style sheets

--- styles/changeTheme.js
+++ styles/changeTheme.js
@@ -1,11 +1,11 @@
 
     document.getElementById('themeButton').addEventListener('click', function() {
-        var stylesheet = document.getElementById('stylesheet');
-        if (stylesheet.getAttribute('href') == './styles/styles.css') {
+        var stylesheet = document.getElementById('stylesheet-theme');
+        if (stylesheet.getAttribute('href') == './styles/style_light.css') {
             stylesheet.setAttribute('href', './styles/style_dark.css');
             // this.innerText = "Switch to Light Mode";
         } else {
-            stylesheet.setAttribute('href', './styles/styles.css');
+            stylesheet.setAttribute('href', './styles/style_light.css');
             // this.innerText = "Switch to Dark Mode";
         }
     });
