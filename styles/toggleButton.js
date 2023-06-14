5db161ca0475f2c9902fe0017fdb5e6d9e8e9f93
Ellen Houghton <ellen.a.ash@gmail.com>
2023-06-14 17:36:49 +0100

    add toggle button js and associated div id in html with css styling

---
+++ styles/toggleButton.js
@@ -0,0 +1,11 @@
+document.getElementById('toggleButton').addEventListener('click', function () {
+    var text = document.getElementById('personal_profile_1');
+    if (text.classList.contains('expanded')) {
+      this.innerHTML = 'show more';
+      text.classList.remove('expanded');
+    } else {
+      this.innerHTML = 'show less';
+      text.classList.add('expanded');
+    }
+  });
+  
