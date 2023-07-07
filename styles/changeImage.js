2ea2fc25c4ae46fb58e7ed00a0d91ddc1f16c1bc
Ellen Houghton <ellen.a.ash@gmail.com>
2023-07-07 15:25:34 +0100

    working js script to switch image for scraped wordcloud of skills

---
+++ styles/changeImage.js
@@ -0,0 +1,4 @@
+document.getElementById('img_switch').addEventListener('click', function() {
+	var img = document.getElementById('skills_cloud');
+	img.src = "./images/EllenHoughtonCVwordcloudJuly23_small.png";
+});
--- styles/changeTheme.js
+++ styles/changeTheme.js
@@ -1,11 +1,12 @@
 
-    document.getElementById('themeButton').addEventListener('click', function() {
-        var stylesheet = document.getElementById('stylesheet-theme');
-        if (stylesheet.getAttribute('href') == './styles/style_light.css') {
-            stylesheet.setAttribute('href', './styles/style_dark.css');
-            // this.innerText = "Switch to Light Mode";
-        } else {
-            stylesheet.setAttribute('href', './styles/style_light.css');
-            // this.innerText = "Switch to Dark Mode";
-        }
-    });
+document.getElementById('themeButton').addEventListener('click', function() {
+    var stylesheet = document.getElementById('stylesheet-theme');
+    if (stylesheet.getAttribute('href') == './styles/style_light.css') {
+        stylesheet.setAttribute('href', './styles/style_dark.css');
+        // this.innerText = "Switch to Light Mode";
+    } else {
+        stylesheet.setAttribute('href', './styles/style_light.css');
+        // this.innerText = "Switch to Dark Mode";
+    }
+});
+
