e160d133147178afce2057d748c71e83089f992d
Ellen Houghton <ellen.a.ash@gmail.com>
2023-10-01 13:35:06 +0100

    remove debt comparison

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -47,7 +47,7 @@ try {
     let sentenceTranslation = project.sentenceTranslation;
     let heathrowHeatmaps = project.heathrowHeatmaps;
     let scraper = project.scraper;
-    projectsCollection = [restapi, yahtzee, scraper, brewqueue, heathrowHeatmaps, symptomLogger, debtComparisonCalculator];
+    projectsCollection = [restapi, yahtzee, scraper, brewqueue, heathrowHeatmaps, symptomLogger];
 
     response = await fetch('./scripts/skills.json');
     let skills = await response.json();
